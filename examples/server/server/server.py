3d25aeb3d6727a1122553b4e16b1f3c0c8c13bbe
rainzhang05 <tza103@sfu.ca>
2025-09-16 16:43:39 +0800

    Using localhost for running on https

--- examples/server/server/server.py
+++ examples/server/server/server.py
@@ -1055,7 +1055,12 @@ def main():
     # Note: using localhost without TLS, as some browsers do
     # not allow Webauthn in case of TLS certificate errors.
     # See https://lists.w3.org/Archives/Public/public-webauthn/2022Nov/0135.html
-    app.run(ssl_context=("localhost+1.pem", "localhost+1-key.pem"))
+    app.run(
+        host="localhost",
+        port=5000,
+        ssl_context=("localhost+1.pem", "localhost+1-key.pem"),
+        debug=True
+    )
 
 if __name__ == "__main__":
     main()
