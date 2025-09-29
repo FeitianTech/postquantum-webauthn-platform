ec2c5de7a59bcaef53b91370755530cf5e6dce3a
Rain Zhang <tza103@sfu.ca>
2025-09-29 09:40:43 +0800

    Fix missing AttestedCredentialData import in advanced routes

--- examples/server/server/routes/advanced.py
+++ examples/server/server/routes/advanced.py
@@ -14,6 +14,7 @@ from typing import Any, Dict, Iterable, List, Mapping, MutableMapping, Optional
 from flask import jsonify, request, session
 from fido2.webauthn import (
     AttestationConveyancePreference,
+    AttestedCredentialData,
     AuthenticatorData,
     AuthenticatorAttachment,
     PublicKeyCredentialDescriptor,
