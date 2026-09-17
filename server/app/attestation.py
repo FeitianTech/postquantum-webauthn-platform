1dc4adf2827ca200c3b07acbf863484d0157ca0c
Rain Zhang <tza103@sfu.ca>
2026-09-17 16:18:24 -0700

    Import the attestation fragments under their own names in the carrier

--- server/app/attestation.py
+++ server/app/attestation.py
@@ -48,33 +48,26 @@ from fido2.webauthn import (
     RegistrationResponse,
 )
 
-from .attestation_parts import aaguid_leaf as _aaguid_leaf
 from .attestation_parts import (
-    certificate_details_runtime as _certificate_details_runtime,
+    aaguid_leaf,
+    certificate_details_runtime,
+    certificate_extensions_leaf,
+    certificate_public_key_leaf,
+    certificate_serialize_runtime,
+    certificate_signature_leaf,
+    certificate_summary_runtime,
+    checks_attestation_runtime,
+    checks_input_runtime,
+    checks_metadata_runtime,
+    checks_policy_runtime,
+    checks_runtime,
+    classical_runtime,
+    encoding_leaf,
+    pqc_constraints_runtime,
+    pqc_runtime,
+    trust_ca_runtime,
+    trust_runtime,
 )
-from .attestation_parts import (
-    certificate_extensions_leaf as _certificate_extensions_leaf,
-)
-from .attestation_parts import (
-    certificate_public_key_leaf as _certificate_public_key_leaf,
-)
-from .attestation_parts import (
-    certificate_serialize_runtime as _certificate_serialize_runtime,
-)
-from .attestation_parts import certificate_signature_leaf as _certificate_signature_leaf
-from .attestation_parts import (
-    certificate_summary_runtime as _certificate_summary_runtime,
-)
-from .attestation_parts import checks_attestation_runtime as _checks_attestation_runtime
-from .attestation_parts import checks_input_runtime as _checks_input_runtime
-from .attestation_parts import checks_metadata_runtime as _checks_metadata_runtime
-from .attestation_parts import checks_policy_runtime as _checks_policy_runtime
-from .attestation_parts import checks_runtime as _checks_runtime
-from .attestation_parts import classical_runtime as _classical_runtime
-from .attestation_parts import pqc_constraints_runtime as _pqc_constraints_runtime
-from .attestation_parts import pqc_runtime as _pqc_runtime
-from .attestation_parts import trust_ca_runtime as _trust_ca_runtime
-from .attestation_parts import trust_runtime as _trust_runtime
 from .attestation_parts.aaguid_leaf import CRED_PROTECT_LABELS
 from .attestation_parts.encoding_leaf import (
     colon_hex,
@@ -138,7 +131,7 @@ EXTENSION_DISPLAY_METADATA: dict[str, dict[str, Any]] = {
 _PQC_ALGORITHM_NAME_TO_ID = {
     name.lower(): alg_id for alg_id, name in PQC_ALGORITHM_ID_TO_NAME.items()
 }
-_HASH_NORMALISE_PATTERN = _certificate_signature_leaf._HASH_NORMALISE_PATTERN
+_HASH_NORMALISE_PATTERN = certificate_signature_leaf._HASH_NORMALISE_PATTERN
 
 _RUNTIME_REBOUND_CACHE: dict[Callable[..., Any], Callable[..., Any]] = {}
 
@@ -171,82 +164,82 @@ def _install_runtime_bindings(bindings: Mapping[str, Callable[..., Any]]) -> Non
 
 
 _AAGUID_LEAF_BINDINGS: dict[str, Callable[..., Any]] = {
-    "describe_cred_protect": _aaguid_leaf.describe_cred_protect,
-    "coerce_non_negative_int": _aaguid_leaf.coerce_non_negative_int,
-    "normalize_aaguid_string": _aaguid_leaf.normalize_aaguid_string,
-    "coerce_aaguid_hex": _aaguid_leaf.coerce_aaguid_hex,
-    "augment_aaguid_fields": _aaguid_leaf.augment_aaguid_fields,
-    "extract_min_pin_length": _aaguid_leaf.extract_min_pin_length,
-    "summarize_authenticator_extensions": _aaguid_leaf.summarize_authenticator_extensions,
+    "describe_cred_protect": aaguid_leaf.describe_cred_protect,
+    "coerce_non_negative_int": aaguid_leaf.coerce_non_negative_int,
+    "normalize_aaguid_string": aaguid_leaf.normalize_aaguid_string,
+    "coerce_aaguid_hex": aaguid_leaf.coerce_aaguid_hex,
+    "augment_aaguid_fields": aaguid_leaf.augment_aaguid_fields,
+    "extract_min_pin_length": aaguid_leaf.extract_min_pin_length,
+    "summarize_authenticator_extensions": aaguid_leaf.summarize_authenticator_extensions,
 }
 
 _TRUST_RUNTIME_BINDINGS: dict[str, Callable[..., Any]] = {
-    "_ensure_utc_datetime": _trust_runtime._ensure_utc_datetime,
-    "_certificate_datetime": _trust_runtime._certificate_datetime,
-    "_coerce_bytes": _trust_runtime._coerce_bytes,
-    "_collect_trust_path_entries": _trust_runtime._collect_trust_path_entries,
-    "_extract_certificate_aaguid": _trust_runtime._extract_certificate_aaguid,
-    "_coerce_certificate_bytes": _trust_runtime._coerce_certificate_bytes,
-    "_extract_attestation_leaf_certificate": _trust_runtime._extract_attestation_leaf_certificate,
-    "_collect_metadata_root_certificates": _trust_runtime._collect_metadata_root_certificates,
-    "_find_metadata_entry_for_aaguid": _trust_runtime._find_metadata_entry_for_aaguid,
-    "_resolve_root_validity": _trust_runtime._resolve_root_validity,
-    "_describe_certificate_subject": _trust_runtime._describe_certificate_subject,
+    "_ensure_utc_datetime": trust_runtime._ensure_utc_datetime,
+    "_certificate_datetime": trust_runtime._certificate_datetime,
+    "_coerce_bytes": trust_runtime._coerce_bytes,
+    "_collect_trust_path_entries": trust_runtime._collect_trust_path_entries,
+    "_extract_certificate_aaguid": trust_runtime._extract_certificate_aaguid,
+    "_coerce_certificate_bytes": trust_runtime._coerce_certificate_bytes,
+    "_extract_attestation_leaf_certificate": trust_runtime._extract_attestation_leaf_certificate,
+    "_collect_metadata_root_certificates": trust_runtime._collect_metadata_root_certificates,
+    "_find_metadata_entry_for_aaguid": trust_runtime._find_metadata_entry_for_aaguid,
+    "_resolve_root_validity": trust_runtime._resolve_root_validity,
+    "_describe_certificate_subject": trust_runtime._describe_certificate_subject,
 }
 
 _TRUST_CA_RUNTIME_BINDINGS: dict[str, Callable[..., Any]] = {
-    "_trusted_ca_subjects": _trust_ca_runtime._trusted_ca_subjects,
-    "_trusted_ca_fingerprints": _trust_ca_runtime._trusted_ca_fingerprints,
-    "_certificate_fingerprint": _trust_ca_runtime._certificate_fingerprint,
-    "_is_trusted_ca_certificate": _trust_ca_runtime._is_trusted_ca_certificate,
+    "_trusted_ca_subjects": trust_ca_runtime._trusted_ca_subjects,
+    "_trusted_ca_fingerprints": trust_ca_runtime._trusted_ca_fingerprints,
+    "_certificate_fingerprint": trust_ca_runtime._certificate_fingerprint,
+    "_is_trusted_ca_certificate": trust_ca_runtime._is_trusted_ca_certificate,
 }
 
 _PQC_RUNTIME_BINDINGS: dict[str, Callable[..., Any]] = {
-    "_evaluate_mldsa_attestation_root": _pqc_runtime._evaluate_mldsa_attestation_root,
-    "_attempt_pqc_attestation_signature_validation": _pqc_runtime._attempt_pqc_attestation_signature_validation,
+    "_evaluate_mldsa_attestation_root": pqc_runtime._evaluate_mldsa_attestation_root,
+    "_attempt_pqc_attestation_signature_validation": pqc_runtime._attempt_pqc_attestation_signature_validation,
 }
 
 _PQC_CONSTRAINTS_RUNTIME_BINDINGS: dict[str, Callable[..., Any]] = {
-    "_normalise_pqc_algorithm_identifier": _pqc_constraints_runtime._normalise_pqc_algorithm_identifier,
-    "_check_pqc_certificate_constraints": _pqc_constraints_runtime._check_pqc_certificate_constraints,
-    "_verify_pqc_attestation_chain": _pqc_constraints_runtime._verify_pqc_attestation_chain,
+    "_normalise_pqc_algorithm_identifier": pqc_constraints_runtime._normalise_pqc_algorithm_identifier,
+    "_check_pqc_certificate_constraints": pqc_constraints_runtime._check_pqc_certificate_constraints,
+    "_verify_pqc_attestation_chain": pqc_constraints_runtime._verify_pqc_attestation_chain,
 }
 
 _CLASSICAL_RUNTIME_BINDINGS: dict[str, Callable[..., Any]] = {
-    "_evaluate_classical_attestation_root": _classical_runtime._evaluate_classical_attestation_root,
+    "_evaluate_classical_attestation_root": classical_runtime._evaluate_classical_attestation_root,
 }
 
 _CERTIFICATE_BINDINGS: dict[str, Callable[..., Any]] = {
-    "format_x509_name": _certificate_signature_leaf.format_x509_name,
-    "_format_algorithm_component": _certificate_signature_leaf._format_algorithm_component,
-    "_format_hash_value": _certificate_signature_leaf._format_hash_value,
-    "_normalise_signature_algorithm_name": _certificate_signature_leaf._normalise_signature_algorithm_name,
-    "_derive_certificate_algorithm_info": _certificate_signature_leaf._derive_certificate_algorithm_info,
-    "_extract_common_names": _certificate_signature_leaf._extract_common_names,
-    "_build_unknown_public_key_info": _certificate_public_key_leaf._build_unknown_public_key_info,
-    "_serialize_public_key_info": _certificate_public_key_leaf._serialize_public_key_info,
-    "_serialize_extension_value": _certificate_extensions_leaf._serialize_extension_value,
-    "_parse_fido_transport_bitfield": _certificate_extensions_leaf._parse_fido_transport_bitfield,
-    "_build_certificate_summary": _certificate_summary_runtime._build_certificate_summary,
-    "_coerce_attestation_certificate_bytes": _certificate_details_runtime._coerce_attestation_certificate_bytes,
-    "extract_attestation_details": _certificate_details_runtime.extract_attestation_details,
-    "_serialize_attestation_certificate_fallback": _certificate_serialize_runtime._serialize_attestation_certificate_fallback,
-    "serialize_attestation_certificate": _certificate_serialize_runtime.serialize_attestation_certificate,
+    "format_x509_name": certificate_signature_leaf.format_x509_name,
+    "_format_algorithm_component": certificate_signature_leaf._format_algorithm_component,
+    "_format_hash_value": certificate_signature_leaf._format_hash_value,
+    "_normalise_signature_algorithm_name": certificate_signature_leaf._normalise_signature_algorithm_name,
+    "_derive_certificate_algorithm_info": certificate_signature_leaf._derive_certificate_algorithm_info,
+    "_extract_common_names": certificate_signature_leaf._extract_common_names,
+    "_build_unknown_public_key_info": certificate_public_key_leaf._build_unknown_public_key_info,
+    "_serialize_public_key_info": certificate_public_key_leaf._serialize_public_key_info,
+    "_serialize_extension_value": certificate_extensions_leaf._serialize_extension_value,
+    "_parse_fido_transport_bitfield": certificate_extensions_leaf._parse_fido_transport_bitfield,
+    "_build_certificate_summary": certificate_summary_runtime._build_certificate_summary,
+    "_coerce_attestation_certificate_bytes": certificate_details_runtime._coerce_attestation_certificate_bytes,
+    "extract_attestation_details": certificate_details_runtime.extract_attestation_details,
+    "_serialize_attestation_certificate_fallback": certificate_serialize_runtime._serialize_attestation_certificate_fallback,
+    "serialize_attestation_certificate": certificate_serialize_runtime.serialize_attestation_certificate,
 }
 
 _CHECKS_BINDINGS: dict[str, Callable[..., Any]] = {
-    "_coerce_expected_bytes": _checks_input_runtime._coerce_expected_bytes,
-    "_resolve_expected_challenge": _checks_input_runtime._resolve_expected_challenge,
-    "_populate_client_data_results": _checks_input_runtime._populate_client_data_results,
-    "_populate_rp_id_hash_result": _checks_input_runtime._populate_rp_id_hash_result,
-    "_resolve_uv_required": _checks_policy_runtime._resolve_uv_required,
-    "_collect_allowed_algorithms": _checks_policy_runtime._collect_allowed_algorithms,
-    "_populate_authenticator_data_results": _checks_input_runtime._populate_authenticator_data_results,
-    "_resolve_signature_validation": _checks_attestation_runtime._resolve_signature_validation,
-    "_collect_attestation_trust_path": _checks_attestation_runtime._collect_attestation_trust_path,
-    "_evaluate_root_validation": _checks_attestation_runtime._evaluate_root_validation,
-    "_finalize_metadata_results": _checks_metadata_runtime._finalize_metadata_results,
-    "perform_attestation_checks": _checks_runtime.perform_attestation_checks,
+    "_coerce_expected_bytes": checks_input_runtime._coerce_expected_bytes,
+    "_resolve_expected_challenge": checks_input_runtime._resolve_expected_challenge,
+    "_populate_client_data_results": checks_input_runtime._populate_client_data_results,
+    "_populate_rp_id_hash_result": checks_input_runtime._populate_rp_id_hash_result,
+    "_resolve_uv_required": checks_policy_runtime._resolve_uv_required,
+    "_collect_allowed_algorithms": checks_policy_runtime._collect_allowed_algorithms,
+    "_populate_authenticator_data_results": checks_input_runtime._populate_authenticator_data_results,
+    "_resolve_signature_validation": checks_attestation_runtime._resolve_signature_validation,
+    "_collect_attestation_trust_path": checks_attestation_runtime._collect_attestation_trust_path,
+    "_evaluate_root_validation": checks_attestation_runtime._evaluate_root_validation,
+    "_finalize_metadata_results": checks_metadata_runtime._finalize_metadata_results,
+    "perform_attestation_checks": checks_runtime.perform_attestation_checks,
 }
 
 _install_runtime_bindings(
